184cd7d2f641702beb2be1ce0522973d5d7ec064
kiri <pruprublue@gmail.com>
2025-10-14 02:41:59 +0800

    continue fix some bug

--- Assets/Scripts/DialogueSystem/DialogueBridge.cs
+++ Assets/Scripts/DialogueSystem/DialogueBridge.cs
@@ -47,6 +47,7 @@ public class DialogueBridge : MonoBehaviour
             Debug.Log($"尝试解锁行为: {behaviorName}");
             // 在这里实现具体的行为解锁逻辑
             // 例如: PetManager.Instance.ActivePet.BehaviorRegistry.Unlock(behaviorName);
+            PetManager.Instance.ActivePet.BehaviorRegistry.Unlock(behaviorName);
         }
     }
 
